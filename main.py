0e022e07de06306ffb0babcfae37cb7e446c4a93
Edmund Leibert III <edmund.leibert@outlook.com>
2023-09-04 11:44:04 -0700

    🚧 dev[wip]: Working streamlit application, but it is not interactive
    • Need to make the application more interactive

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def kadanes_algorithm(arr):
+    max_current = max_global = arr[0]
+    start = end = 0
+    temp_start = 0
 
+    for i in range(1, len(arr)):
+        if arr[i] > max_current + arr[i]:
+            max_current = arr[i]
+            temp_start = i
+        else:
+            max_current += arr[i]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        if max_current > max_global:
+            max_global = max_current
+            start = temp_start
+            end = i
 
+    return max_global, start, end
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+st.title("Kadane's Algorithm Visualization")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# User input for array
+user_input = st.text_input("Enter a list of integers separated by commas:", "1, -2, 3, 4, -1, 2, 1, -5, 4")
+arr = list(map(int, user_input.split(',')))
+
+# Calculate using Kadane's Algorithm
+max_sum, start, end = kadanes_algorithm(arr)
+st.write(f"Largest Sum Contiguous Subarray: {arr[start:end+1]}")
+st.write(f"Sum: {max_sum}")
+
+# Visualization using Matplotlib
+plt.figure(figsize=(10, 5))
+plt.axhline(0, color="gray", lw=0.5)
+plt.plot(arr, "o-", label="Array elements")
+plt.fill_between(range(start, end + 1), arr[start:end + 1], color="green", alpha=0.5, label="Max Sum Subarray")
+plt.title("Kadane's Algorithm Visualization")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.legend()
+
+# Show plot in Streamlit
+st.pyplot(plt.gcf())
